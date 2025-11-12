df7263104a6453ebbbe74b64b902cfbfff91f298
nerdymaxx <nerdymax84@gmail.com>
2025-11-12 03:31:06 +0100

    teacing on basic operations

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,11 @@
+number1 = 10
+number2 = 5
+
+sum = number1 + number2 
+print(f"addition of {number1} and {number2} is {sum}")
+
+subtraction= number1 - number2
+print(f"subtraction of {number1} and {number2} is {subtraction}")
+
+multiplication = number1 * number2
+print(f"multiplication of {number1} and {number2} is {multiplication}") 
