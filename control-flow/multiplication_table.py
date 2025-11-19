ae3eb6e3b642d036fefce4437fc3aed5b37de31d
nerdymaxx <nerdymax84@gmail.com>
2025-11-19 15:54:18 +0100

    corrected some errors

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -3,6 +3,6 @@ user_number = int(input("Enter a number to see its multiplication table: "))
 numbers= 1,2,3,4,5,6,7,8,9,10
 print(numbers)
 
-for number in  numbers:
+for number in  range(1,10):
     result = number * user_number
     print(f"{user_number} * {number} = {result}" )
