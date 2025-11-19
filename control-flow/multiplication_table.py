db947fdcca03864afcf8172be7d1d7f38a560dc5
nerdymaxx <nerdymax84@gmail.com>
2025-11-19 15:55:57 +0100

    changed to range

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,7 +1,5 @@
 user_number = int(input("Enter a number to see its multiplication table: "))
 
-numbers= 1,2,3,4,5,6,7,8,9,10
-print(numbers)
 
 for number in  range(1,10):
     result = number * user_number
